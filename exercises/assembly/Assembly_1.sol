c1fc47fd94e78c8d4348d64cdba32ea5d675a916
Raktim Shrestha <rakimsth@gmail.com>
2024-09-04 22:50:13 +0545

    solved ex 1

--- exercises/assembly/Assembly_1.sol
+++ exercises/assembly/Assembly_1.sol
@@ -1,17 +1,21 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 contract Intro {
     function intro() public pure returns (uint16) {
-        uint256 mol = 420;
+        uint256 mol = 999999999999999999420;
 
         // Yul assembly magic happens within assembly{} section
         assembly {
-            // stack variables are instantiated with
-            // let variable_name := VALßUE
-            // instantiate a stack variable that holds the value of mol
-            // To return it needs to be stored in memory
-            // with command mstore(MEMORY_LOCATION, STACK_VARIABLE)
-            // to return you need to specify address and the size from the starting point
+            // Stack variables are instantiated with "let variable_name := VALUE"
+            let stackVar := mol
+
+            // To return it needs to be stored in memory with "mstore(MEMORY_LOCATION, STACK_VARIABLE)"
+            mstore(0x80, stackVar)
+
+            // To return, you need to specify the address and the size from the starting point
+            // Returning a 16-bit (2-byte) value, so the size is 0x20 (32 bytes) and the memory location is 0x80
+            return(0x80, 0x20)
         }
     }
 }
