75c51d8ce1e26722e1da9dc6f585c0052b000629
Raktim Shrestha <rakimsth@gmail.com>
2024-09-04 22:50:19 +0545

    solved ex 2

--- exercises/assembly/Assembly_2.sol
+++ exercises/assembly/Assembly_2.sol
@@ -1,18 +1,17 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 contract Add {
     function addAssembly(uint256 x, uint256 y) public pure returns (uint256) {
-        // Intermediate variables can't communicate between  assembly blocks
-        // But they can be written to memory in one block
-        // and retrieved in another.
-        // Fix this code using memory to store the result between the blocks
-        // and return the result from the second block
         assembly {
             let result := add(x, y)
+            let fmp := mload(0x40)
+            mstore(fmp, result)
         }
 
         assembly {
-            return()
+            let fmp := mload(0x40)
+            return(fmp, 32)
         }
     }
 }
