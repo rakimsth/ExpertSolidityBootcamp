a11ac47de7dd1f5515916265de825d8ea62759da
Raktim Shrestha <rakimsth@gmail.com>
2024-09-04 22:52:33 +0545

    solved ex 4

--- exercises/assembly/Assembly_4.sol
+++ exercises/assembly/Assembly_4.sol
@@ -1,13 +1,21 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 contract Scope {
     uint256 public count = 10;
 
     function increment(uint256 num) public {
-        // Modify state of the count variable from within
+        // Modify the state of the count variable from within
         // the assembly segment
         assembly {
+            // Load the storage slot of `count` which is at slot 0
+            let countSlot := sload(0)
 
+            // Increment the value by the provided `num`
+            let newCount := add(countSlot, num)
+
+            // Store the new value back into the `count` slot (slot 0)
+            sstore(0, newCount)
         }
     }
 }
